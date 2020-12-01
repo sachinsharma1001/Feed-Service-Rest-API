cf768a3af471390f70c4d7291e07022a3c4c14e3
sachinsharma1001 <sachin.sharma100197@gmail.com>
2020-12-01 13:26:38 +0530

    Converting feed to microservices

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { sequelize } from './sequelize';
+import bodyParser from 'body-parser';
+import { FeedRouter } from './controller/feeds/routes/feed.router';
+import { FeedItem } from './controller/feeds/models/FeedItem';
+
+(async () => {
+  await sequelize.addModels([FeedItem]);
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080; // default port to listen
+  
+  app.use(bodyParser.json());
+
+  //CORS Should be restricted
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+
+  app.use('/api/v0/', FeedRouter)
+
+  // Root URI call
+  app.get( "/", async ( req, res ) => {
+    res.send( "/api/v0/" );
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
