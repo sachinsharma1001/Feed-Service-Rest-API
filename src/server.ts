9e0616d2c6045be3ba6199a90faaa763c198a9c1
sachinsharma1001 <sachin.sharma100197@gmail.com>
2020-12-07 18:31:25 +0530

    changing port number

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { FeedItem } from './controller/feeds/models/FeedItem';
   await sequelize.sync();
 
   const app = express();
-  const port = process.env.PORT || 8080; // default port to listen
+  const port = 8080; // default port to listen
   
   app.use(bodyParser.json());
 
